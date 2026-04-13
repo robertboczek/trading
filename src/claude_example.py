8f9c3a68959726207c50f6af72f567116d68a110
Robert Boczek <robertboczek@Roberts-MacBook-Pro.local>
2026-04-12 23:50:18 -0700

    Claude earnings report analyzer

--- src/claude_example.py
+++ src/claude_example.py
@@ -4,8 +4,13 @@ import fitz  # PyMuPDF
 import requests
 
 # download the earnings report for a specific company and quarter (e.g., AAPL Q4 2023)
-url = "https://s22.q4cdn.com/959853165/files/doc_financials/2025/q4/FINAL-Q4-25-Shareholder-Letter.pdf"
+url = "https://s206.q4cdn.com/479360582/files/doc_financials/2025/q4/2025q4-alphabet-earnings-release.pdf"
+#"https://s22.q4cdn.com/959853165/files/doc_financials/2025/q4/FINAL-Q4-25-Shareholder-Letter.pdf"
 pdf_file = "downloaded_file.pdf"
+
+google_earnings1 = "https://s206.q4cdn.com/479360582/files/doc_financials/2025/q1/2025q1-alphabet-earnings-release.pdf"
+google_earnings2 = "https://s206.q4cdn.com/479360582/files/doc_financials/2024/q1/2024q1-alphabet-earnings-release-pdf.pdf"
+google_earnings3 = "https://s206.q4cdn.com/479360582/files/doc_financials/2023/q2/2023q2-alphabet-earnings-release.pdf"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
@@ -36,7 +41,8 @@ message = client.messages.create(
     model="claude-opus-4-6", # Select your model (e.g., Sonnet or Opus)
     max_tokens=1024,
     messages=[
-        {"role": "user", "content": f"Can you extract Q4 revenue from earnings report: <report>{earnings_report_content}</report>? Respond with json format: {{\"revenue\": \"value\"}} and nothing else."}
+        #{"role": "user", "content": f"Can you extract Q4 revenue from earnings report: <report>{earnings_report_content}</report>? Respond with json format: {{\"revenue\": \"value\"}} and nothing else."}
+        {"role": "user", "content": f"Given these Q4 earnings results from earnings report: <report>{earnings_report_content}</report> given expected EPS was $2.57-$2.64 and expected Q4 revenue was $111.43B. Apart from raw revenue and EPS analyze capex compared to previous periods and whether it's positive or negative for the stock? Think as a real-time trading investor, would you buy this stock immediately based on these results? Respond with simple text yes or no where yes indicates investors to react positively and no otherwise."}
     ]
 )
 print(message.content[0].text)
